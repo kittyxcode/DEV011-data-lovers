6fe34d203777f2073a9b5dfb9923f2c27aed91fc
kittyxcode <katherine.pino059@gmail.com>
2023-09-09 21:32:30 -0300

    Creacion de primer fetch y llamada a archivo JSON ok

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,36 @@
 // estas funciones son de ejemplo
 
+//obtenemos la ruta del archivo JSON
+const rutaArchivoJSON = "../data/pokemon/pokemon.json";
+
+//utilizar fetch para cargar archivo JSON
+// Utiliza fetch para cargar el archivo JSON
+fetch(rutaArchivoJSON)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("No se pudo cargar el archivo JSON.");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // Haz algo con los datos JSON, por ejemplo, console.log(data)
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
 export const example = () => {
-  return 'example';
+  return "example";
 };
 
 export const anotherExample = () => {
   return [];
+};
+
+export const filterData = (data, filterBy, value) => {
+  data;
+  filterBy;
+  value;
+  alert("filterData nomas");
 };
