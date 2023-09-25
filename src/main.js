0a12af212953d9d7f45fdc2c0bff7b5496333c4c
kittyxcode <katherine.pino059@gmail.com>
2023-09-25 14:12:26 -0300

    Menu responsivo de hamburguesa

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import { renderItems } from "./view.js";
 //import { allPokemonChargeScreen } from "./dataFunctions.js";
 
-
 //toda la data
 import data from "./data/pokemon/pokemon.js";
 //capturar elemento dom
-const ulElement= document.querySelector("#tarjetas");
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
 
-const prueba= renderItems(data.pokemon);
+abrir.addEventListener("click", () => {
+  nav.classList.add("visible");
+});
+cerrar.addEventListener("click", () => {
+  nav.classList.remove("visible");
+});
+
+const ulElement = document.querySelector("#tarjetas");
+
+const prueba = renderItems(data.pokemon);
 console.log(prueba);
 //console.log(ulElement);
 //
@@ -15,28 +25,14 @@ ulElement.replaceWith(prueba);
 
 //function LoadAllPokemons(){}
 
-
-
 //renderItems(data);
 
-
-
-
-
 //Manipulacion DOM
-
-
 
 //const selectControll = document.getElementById("select-sort");
 //const radioButtons = document.querySelectorAll('input[name="sort-order"]');
 //const textarea = document.getElementById("textarea");
 const buttonSearch = document.getElementById("buttonSearch");
 
-
 //Manejo de Eventos
-buttonSearch.addEventListener("click", () => {
-
-  
-});
-
-
+buttonSearch.addEventListener("click", () => {});
