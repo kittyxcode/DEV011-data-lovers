e7593c1c970b4d5e2c6f5971899d8487fd8f7ce2
kittyxcode <katherine.pino059@gmail.com>
2023-10-02 10:56:47 -0300

    continuacion trabajo rama...

--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ import data from "./data/pokemon/pokemon.js";
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const ordenByController= document.querySelector("#select-sort");
+
+
+
 let pokemones = data.pokemon;
 
 abrir.addEventListener("click", () => {
@@ -29,11 +33,17 @@ ulElement.replaceWith(prueba);
 
 
 const filtro = document.getElementById("filter-by");
+const btnOrden= document.getElementById("elect-sort");
 
 
 filtro.addEventListener("change", (event) => {
   const tipoPokemon = event.target.value;
   pokemones = filterData(data.pokemon, tipoPokemon);
+
+  const pokemonOrdenados= ordenBy(pokemones, ordenByController.value);
+  console.log(ordenByController.value);
+
+
   const ulElement = document.querySelector("#tarjetas");
   const prueba = renderItems(pokemones);
   ulElement.replaceWith(prueba);
